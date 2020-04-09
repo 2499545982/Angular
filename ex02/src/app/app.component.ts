79cf17ba3103204fadb5b04fd2304141bd7da89c
2499545982 <2499545982@qq.com>
2020-04-09 16:33:42 +0800

    update

--- ex02/src/app/app.component.ts
+++ ex02/src/app/app.component.ts
@@ -15,19 +15,19 @@ export class AppComponent
             new Product(
                 'MYSHOES',
                 'Black Running Shoes',
-                '../assets/image/products/black-shoes.jpg',
+                './assets/image/products/black-shoes.jpg',
                 ['Men', 'Shoes', 'Running Shoes'],
                 109.99),
             new Product(
                 'NEATOJACKET',
                 'Blue Jacket',
-                '../assets/image/products/blue-jacket.jpg',
+                './assets/image/products/blue-jacket.jpg',
                 ['Women', 'Apparel', 'Jackets & Vests'],
                 238.99),
             new Product(
                 'NICEHAT',
                 'A Nice Black Hat',
-                '../assets/image/products/black-hat.jpg',
+                './assets/image/products/black-hat.jpg',
                 ['Men', 'Accessories', 'Hats'],
                 29.99)
         ];
