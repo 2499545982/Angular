6b774548cfde12e580734a1f8530ba8a413df8a3
2499545982 <2499545982@qq.com>
2020-04-16 13:58:56 +0800

    4-16 1

--- ex04/src/app/app.component.ts
+++ ex04/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { MENUS, PRODUCTS } from './data';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ex04';
+export class AppComponent
+{
+    menus = MENUS;
+    products = PRODUCTS;
 }
