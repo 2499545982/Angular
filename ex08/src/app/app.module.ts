26312dfeec2988bb6721aae1f985797760d73276
2499545982 <2499545982@qq.com>
2020-05-17 14:16:32 +0800

    init

---
+++ ex08/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
+import { ManagementComponentComponent } from './management-component/management-component.component';
+import { UserManagementComponentComponent } from './user-management-component/user-management-component.component';
+import { ProductComponentComponent } from './product-component/product-component.component';
+import { ExitComponentComponent } from './exit-component/exit-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponentComponent,
+    LoginComponentComponent,
+    ManagementComponentComponent,
+    UserManagementComponentComponent,
+    ProductComponentComponent,
+    ExitComponentComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
